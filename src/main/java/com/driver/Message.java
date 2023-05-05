7bb2004a2a1d2f991901efd3dd0eb56f99e62857
Sahil Darunde <sahildarunde007@gmail.com>
2023-05-05 14:43:27 +0530

    Assignment Done

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,4 +7,44 @@ public class Message {
     private String content;
     private Date timestamp;
 
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+        // LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int compareTo(Message o)
+    {
+        if(this.timestamp.compareTo(o.timestamp) < 0)
+            return -1;
+        if(this.timestamp.compareTo(o.timestamp) > 0)
+            return 1;
+        return 0;
+    }
 }
